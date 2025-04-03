1fc1c585e4187ce4659ec6ec4295e3651dc3cc2a
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-04-03 19:41:00 +0100

    analysis file layout ideas  outlined

--- analysis.py
+++ analysis.py
@@ -4,3 +4,23 @@
 # author: Kyra Menai Hamilton
 
 # Commencement date: 03/04/2025
+
+# Importing libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# Importing the dataset
+
+
+# Load the dataset from a CSV file
+
+
+# Basic data checks - check for missing values, duplicates, and data types
+
+
+# Summarise each variable in the dataset and check for outliers - export to a single text file.
+
+
+# Histograms, scatter plots 
