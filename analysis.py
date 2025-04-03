5cc5d691190f800d8bdb822b0a873bcb5d433cf1
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-04-03 18:16:46 +0100

    analysis.py created with brief notes on program function

---
+++ analysis.py
@@ -0,0 +1,6 @@
+# analysis.py
+# This program will import and analyse the data from the iris dataset. 
+# Figures will be output and saved as appropriate.
+# author: Kyra Menai Hamilton
+
+# Commencement date: 03/04/2025
