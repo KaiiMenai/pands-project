6e7b9cc18edad6a913d316df99130bb66f4b0f14
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-04-03 20:14:24 +0100

    importing the dataset and naming it, plus naming x and y plus metadata and variable info - print to check it worked

--- analysis.py
+++ analysis.py
@@ -13,6 +13,23 @@ import numpy as np
 
 # Into terminal: pip install ucimlrepo
 
+# Importing the dataset
+from ucimlrepo import fetch_ucirepo 
+
+# fetch dataset 
+iris = fetch_ucirepo(id=53) 
+
+# data (as pandas dataframes) 
+X = iris.data.features 
+y = iris.data.targets 
+
+# metadata 
+print(iris.metadata) 
+
+# variable information 
+print(iris.variables) 
+
+
 
 # Basic data checks - check for missing values, duplicates, and data types
 
