4fb764e0525f48889c8fddbc18564f6a82251a32
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-04-20 13:41:01 +0100

    added observations on missing and data type for the analysis.txt space was added under the sopecies checks for comments

--- analysis.py
+++ analysis.py
@@ -98,7 +98,7 @@ with open("analysis.txt", "w") as file: # The (file=file) argument is important 
     print("Data Analysis conducted on the Iris Dataset", file=file)
     print("The shape of the dataset:", file=file)
     print(iris_df.shape, file=file)
-    print("The dataset contains 150 rows of data and 5 columns. The 5 columns are of the categorical variable that is the species of isis flower (here noted as 'class'), and 4 continuous numerical variables (sepal length, sepal width, petal length, and petal width).", file=file)
+    print("The dataset contains 150 rows of data and 5 columns. The 5 columns are the species of isis flower (here noted as 'class'), and sepal length, sepal width, petal length, and petal width.", file=file)
     print("The first and last five rows of the dataset are printed below, as well as the column names within the dataset.", file=file)
     print("The first 5 rows of the dataset:", file=file)
     print(iris_df.head(), file=file) # This will print the first 5 rows of the dataset.
@@ -118,7 +118,9 @@ with open("analysis.txt", "a") as file:
     print("The number of duplicate rows in the dataset:", file=file)
     print(iris_df.duplicated().sum(), file=file) # This will print the number of duplicate rows in the dataset.
     print("The data types of each column in the dataset:", file=file)
-    print(iris_df.dtypes, file=file) # This will print the data types of each column in the dataset.print("This is the initial content of the file.", file=file)
+    print(iris_df.dtypes, file=file) 
+    print("\nMissing values were checked for in the dataset, there were none. If there were misasing values, the dataset would need to be cleaned and sorted further before any analysis could be conducted. There were no missing values in this dataset, so further cleaning was unnecessary.", file=file)
+    print("\nFrom the information table, it can be seen that where one column has categorical (object) data (class column - also referred to as species for this dataset) and the four other columns (sepal length, sepal width, petal length, and petal width) are of the float type (float64) (continuous variables) with non-Null entries. That is, there are no 0 / null~ entries in the dataset.", file=file)
     
 print("Basic data checks explanation has been appended to analysis.txt")
 
@@ -199,6 +201,8 @@ with open("analysis.txt", "a") as file:
     print("\n")
     print("\nVirginica Statistics:", file=file)
     print(virginica_stats, file=file)
+    print("\n")
+    
     
 print("Summary Stats has been appended to analysis.txt")
 
