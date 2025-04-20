f987dc59851a8e79b6297cf8d589f8936e3b797f
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-04-20 14:08:10 +0100

    duplicates txt cleaned up

--- analysis.py
+++ analysis.py
@@ -187,15 +187,14 @@ print("Checks for data outliers has been appended to summary_statistics.txt")
 
 with open("analysis.txt", "a") as file:
     print("\nDuplicates were removed from the data using the drop_duplicates function.", file=file)
-    print("\nThe code used for this was: \ndata = iris_df.drop_duplicates(subset="'class'".)", file=file)
-    print("Summary statistics for each species:", file=file)
-
+    print("The code used for this was: \ndata = iris_df.drop_duplicates(subset="'class'".)", file=file)
+    print("\nSummary statistics for each species:", file=file)
         # Separate the dataset by species
     setosa_stats = iris_df[iris_df['class'] == 'Iris-setosa'].describe()
     versicolor_stats = iris_df[iris_df['class'] == 'Iris-versicolor'].describe()
     virginica_stats = iris_df[iris_df['class'] == 'Iris-virginica'].describe()
     # Display the statistics for each species
-    print("Setosa Statistics:", file=file)
+    print("\nSetosa Statistics:", file=file)
     print(setosa_stats, file=file)
     print("\nVersicolor Statistics:", file=file)
     print(versicolor_stats, file=file)
