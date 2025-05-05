91b36ddbf1824d873a9997b85b5448bef50eee3d
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-05-05 15:35:11 +0100

    petal width observations

--- analysis.py
+++ analysis.py
@@ -310,7 +310,7 @@ with open("analysis.txt", "a") as file:
     print("\nFrom the histogram plot for sepal length by species, the Setosa species showed a normal distribution, with the majority of sepals being approximately 5.0 cm in length. The Versicolor species has a broad range of sepal lengths, with the most sepals being approximately 5.5 cm in length. The species with the largest range in length of sepals and longest average sepal length is the Virginica species.",  file=file)
     print("\nFor sepal width, it can be seen that overall, the data shows a normal distribution. Contrary to what was observed for sepal length, the narrowest sepal width is the Versicolor species, with the Virginica species being in the middle of the range. The Setosa species had the greatest width and the broadest range in values for sepal width.", file=file)
     print("\nSimilar to what was observed for sepal length, for petal length Setosa was the species with the shortest average length and the smallest range in measurements. An average petal length of approximately 4.5 cm was observed for the Versicolor species and demonstrated a normal distribution. The Virginica species had, on average, the longest petal lengths, similar to what was observed for sepal lengths.", file=file)
-    print("\n", file=file)
+    print("\nSetosa species had the narrowest petal width on average. The species with the mid-width measurement was the Versicolor species, with values between 1.0 cm and 2.0 cm. The widest petal widths were observed in the Virginica species.", file=file)
     print("\n", file=file)
     
 # Scatter plots - plot and save scatter plots for each pair of variables in the dataset as a png file.
